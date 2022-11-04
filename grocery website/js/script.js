f70858ba92818ec562ba5d4668deba89c76f6443
Anushka4546 <55707780+Anushka4546@users.noreply.github.com>
2022-11-04 20:02:58 +0530

    adding payment

--- grocery website/js/script.js
+++ grocery website/js/script.js
@@ -133,6 +133,27 @@ function addItemToCart(title, price, imageSrc) {
 }
 
 function checkoutClicked() {
+    fetch('http://localhost:3000/create-checkout-session', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            items: [
+                {id: 1, quantity: 3},
+                {id: 2, quantity: 1}
+            ]
+        })
+    }).then(res => {
+        if(res.ok) return res.json()
+        return res.json().then(json => Promise.reject(json))
+    }).then(({ url }) => {
+        window.location = url
+        // console.log(url);
+    }).catch(e => {
+        console.log(e.error)
+    })
+
     alert("Thank you for shopping with us!!");
     var cartItems = document.getElementsByClassName("cart-items")[0];
 
